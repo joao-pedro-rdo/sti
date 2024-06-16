a553fe12dc5cb9072cf18839e2170680f728f1e9
João Pedro Ramos <122635721+joao-pedro-rdo@users.noreply.github.com>
2024-06-15 21:03:05 -0300

    🐛[FIX] Update glpi-install.sh

--- glpi-install.sh
+++ glpi-install.sh
@@ -3,18 +3,18 @@
 #instalando dependencia de entrada de usuario
 sudo apt-get install expect -y
 
-# Definindo a URL do agente GLPI. Certifique-se de verificar a versão mais recente no site oficial.
+# Definindo a URL do agente GLPI. Certifique-se de verificar a versÃ£o mais recente no site oficial.
 GLPI_AGENT_URL="https://github.com/glpi-project/glpi-agent/releases/download/1.9/glpi-agent-1.9-linux-installer.pl"
 
 
-# Diretório temporário para download
+# DiretÃ³rio temporÃ¡rio para download
 TEMP_DIR="/tmp/glpi-agent-install"
-# Configuração básica do GLPI Agent
+# ConfiguraÃ§Ã£o bÃ¡sica do GLPI Agent
 GLPI_SERVER_URL="http://10.24.125.11/glpi/front/inventory.php"
 
-# Editar o arquivo de configuração do GLPI Agent
+# Editar o arquivo de configuraÃ§Ã£o do GLPI Agent
 CONFIG_FILE="/etc/glpi-agent/glpi-agent.conf"
-# Criar o diretório temporário
+# Criar o diretÃ³rio temporÃ¡rio
 mkdir -p $TEMP_DIR
 cd $TEMP_DIR
 
@@ -28,27 +28,30 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Tornando o instalador executável
+# Tornando o instalador executÃ¡vel
 chmod +x glpi-agent-installer.pl
 
 # Executando o instalador
 echo "Instalando o GLPI Agent..."
-sudo perl glpi-agent-installer.pl
+#sudo perl glpi-agent-installer.pl
 
 #Entrada de usuario automatizado
+#talvez precise mudar para perl o ./
 expect <<EOF
 spawn ./glpi-agent-installer.pl
-expect "Enter the URL of the GLPI server"
+expect "Provide an url to configure GLPI server:" 
 send "$GLPI_SERVER_URL\r"
+expect "Provide a path to configure local inventory run or leave it empty:"
+send "/tmp\r"
 expect eof
 EOF
-# Verificando se a instalação foi bem-sucedida
+# Verificando se a instalaÃ§Ã£o foi bem-sucedida
 if [ $? -ne 0 ]; then
-    echo "Erro durante a instalação do GLPI Agent."
+    echo "Erro durante a instalaÃ§Ã£o do GLPI Agent."
     exit 1
 fi
 
-# Limpar o diretório temporário
+# Limpar o diretÃ³rio temporÃ¡rio
 rm -rf $TEMP_DIR
 
 # Exibindo mensagem de sucesso
@@ -57,23 +60,22 @@ echo "GLPI Agent instalado com sucesso!"
 #echo "Configurando o GLPI Agent para se comunicar com o servidor GLPI em $GLPI_SERVER_URL..."
 #if [ -f $CONFIG_FILE ]; then
 #    sudo sed -i "s|^# server=.*|server=$GLPI_SERVER_URL|" $CONFIG_FILE
-#    echo "Configuração do servidor GLPI atualizada com sucesso."
+#    echo "ConfiguraÃ§Ã£o do servidor GLPI atualizada com sucesso."
 #else
-#    echo "Arquivo de configuração $CONFIG_FILE não encontrado."
+#    echo "Arquivo de configuraÃ§Ã£o $CONFIG_FILE nÃ£o encontrado."
 #    exit 1
 #fi
 
-# Reiniciar o serviço do GLPI Agent para aplicar as mudanças
-#echo "Reiniciando o serviço do GLPI Agent..."
+# Reiniciar o serviÃ§o do GLPI Agent para aplicar as mudanÃ§as
+#echo "Reiniciando o serviÃ§o do GLPI Agent..."
 #sudo systemctl restart glpi-agent
 
-# Verificando se o serviço está ativo
+# Verificando se o serviÃ§o estÃ¡ ativo
 #sudo systemctl status glpi-agent --no-pager
 
-echo "Configuração concluída com sucesso!"
+echo "ConfiguraÃ§Ã£o concluÃ­da com sucesso!"
 
-echo "Forçando inventario"
+echo "ForÃ§ando inventario"
 sudo glpi-agent
 
-ehco "Fim da instalação"
-
+echo "Fim da instalaÃ§Ã£o"
